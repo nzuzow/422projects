194121163412922136bddb67912fe9475ed64909
Nick <zuzownic@msu.edu>
2014-11-17 00:32:08 -0500

    Removed some unnecessary comments. Also added a small amount more to the section where we receive the message body.

--- Project3/stream_client.cc
+++ Project3/stream_client.cc
@@ -151,28 +151,8 @@ int main(int argc, char* argv[])
   // server.
   string response_header, response_body;
 
-  // Setup a buffer array and value for the total length of the received message.
-  // The value for the buffer came from the value within test_client. I'm not
-  // actually sure why this value was picked though.
-  //char buff[65536];
-  //int total_received_len = 0;
-
   // Now call read_header to get the proper information from the socket
   client_sock.read_header(response_header, response_body);
-
-  // Actually need to call receive_response_headers since we don't have one
-  // header followed by one large body. This method takes three arguments,
-  // the buffer to hold the data in, the maximum length of the buffer, and
-  // the total number of bytes received so far.
-  //int header_stop = client_sock.receive_response_headers(buff, 65536, total_received_len);
-
-  // Make sure the end position of the header is not 0 meaning the header is
-  // not empty
-  //if( header_stop <= 0)
-  //{
-  //  cout << "There was an error receiving the response from the server." << endl;
-  //  exit(1);
-  //}
 
 
   // The HTTP_Response::parse construct a response object, and check if
@@ -227,10 +207,22 @@ int main(int argc, char* argv[])
     //s >> response->content_len;
   }
 
+  // Do a check to see if content length is greater than 0, which will
+  // signify if it has been changed or not.
+  if( content_len <= 0)
+  {
+    cout << "Error, no content was downloaded." << endl;
+    exit(1);
+  }
+
   /*** END OF RECEIVE RESPONSE HEADER FROM SERVER ***/
 
 
   /*** GET REST OF THE MESSAGE BODY AND STORE IT ***/
+
+  /* I'm not actually sure if we need to open a new file here or not. I will
+  look at this more tomorrow. */
+  
   // Open a local copy in which to store the file.
   out = Open_local_copy(server_uri);
   // check
@@ -241,6 +233,12 @@ int main(int argc, char* argv[])
     delete server_uri;
     exit(1);
   }
+
+  // Define some variables to use in downloading the content
+  int bytes_written = 0;
+  int bytes_left;
+  int total_data;
+
 
   /*** END OF GETTING THE REST OF THE MESSAGE BODY AND STORING IT ***/
 
